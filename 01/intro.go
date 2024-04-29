3dcf4a3c3636d5aca0dc308e498ea214b47cc9ca
Pablo Diosquez <pablodiosquez24@gmail.com>
2024-04-29 14:47:30 -0300

    'intro.go' was updated to the repository

--- 01/intro.go
+++ 01/intro.go
@@ -4,4 +4,21 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hola Mundo!")
+
+	a, b := 5, 9
+	fmt.Println("a:", a)
+	fmt.Println("b:", b)
+
+	a, b = b, a
+
+	var (
+		x int     = 10
+		y float64 = 12.45
+		z uint    = 3
+	)
+
+	fmt.Println("x: ", x)
+	fmt.Println("y: ", y)
+	fmt.Println("z: ", z)
+
 }
